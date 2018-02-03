ef3f222e120d79b3afdc702a71b6d9a900e35dc4
leo-recover <leoricupero@gmail.com>
2018-02-03 19:19:11 +0100

    Initial rework of radio driver

--- firmware/smart_node/src/drivers/radio.c
+++ firmware/smart_node/src/drivers/radio.c
@@ -1,87 +1,85 @@
 /**
- * @file nRF24L01.c
+ * @file radio.c
  *
  * @date 22/09/2014 18:30:05
  * @authors Stefan Engelke, Leonardo Ricupero
  */ 
 
 #include "micro.h"
+#include "usart.h"
 #include "radio.h"
 
+#define DEFAULT_ADDRESS_SIZE 5
+#define DEFAULT_NODE_ADDRESS {0x00, 0x01, 0x02, 0x03, 0x04}
+
+#define RADIO_DRIVE_CE_LOW()  {PORTB &= ~(1<<PORTB1);}
+#define RADIO_DRIVE_CE_HIGH() {PORTB |= (1<<PORTB1);}
+
+static uint8_t Node_Address[DEFAULT_ADDRESS_SIZE] = DEFAULT_NODE_ADDRESS;
+
+static void WriteRegister(uint8_t reg, uint8_t value);
+
+uint8_t* data;
+
 /**
- * \brief Setup the RF24 module
+ * Setup the RF24 module
  * 
- * Edit this function in order to change the initial configuration
- * of the radio module
+ * @brief Edit this function in order to change the initial configuration
+ *        of the radio module
  * 
- * \return void
  */
 void Radio__Initialize(void)
 {
-	uint8_t val[5];
+	uint8_t val;
 	// Set CE low to start with, because nothing has to be transmitted
-    PORTB &= ~(1<<PORTB1);
+    RADIO_DRIVE_CE_LOW();
 	
 	// EN_AA - (enable auto-acknowledgments)
 	// Transmitter gets automatic response from receiver in case of successful transmission
 	// It only works if the TX module has the same RF_Address on its channel. ex: RX_ADDR_P0 = TX_ADDR
-	val[0] = 0x03;	
-	RF24ReadWrite(W, EN_AA, val, 1);
+    val = (1 << BIT_ENAA_P0);
+	WriteRegister(REG_EN_AA, &val, 1);
 	
 	// SETUP_RETR (the setup for "EN_AA")
 	// 0b0010 00011 "2" sets it up to 750uS delay between every retry (at least 500us at 250kbps and if payload >5bytes in 1Mbps, and if payload >15byte in 2Mbps) "F" is number of retries (1-15, now 15)
-	val[0] = 0x2F;
-	RF24ReadWrite(W, SETUP_RETR, val, 1);
+	val = (2 << BIT_ARD) | (15 << BIT_ARC); //0x2F;
+	WriteRegister(REG_SETUP_RETR, &val, 1);
 	
 	// Choose the number of the enabled RX data pipe (0-5)
-	val[0] = 0x03;
-	RF24ReadWrite(W, EN_RXADDR, val, 1); // enable data pipe 0 and 1
+	val = (1 << BIT_ERX_P0);
+	WriteRegister(REG_EN_RXADDR, &val, 1); // enable data pipe 0
 	
 	// RF_Address width setup: how many bytes is the receiver address
-	val[0] = 0x03;
-	RF24ReadWrite(W, SETUP_AW, val, 1); // 0b0000 00011 5byte RF_Adress
+	val = (0x03 << BIT_AW);
+	WriteRegister(REG_SETUP_AW, &val, 1); // 5byte RF Address
 	
 	// RF channel setup - choose frequency 2.401 - 2.527 GHz, 1 MHz/step
-	val[0] = 0x4C;
-	RF24ReadWrite(W, RF_CH, val, 1); // 0b1101 0100 = 2,476 GHz (same on TX and RX)
+	val = 0x4C;
+	WriteRegister(REG_RF_CH, &val, 1); // 0b1101 0100 = 2,476 GHz (same on TX and RX)
 	
 	//RF setup - choose power mode and data speed
-	val[0] = 0x07;
-	RF24ReadWrite(W, RF_SETUP, val, 1); //0b0000 0111 bit 3="0" 1Mbps=longer range, bit 2-1 power mode ("11" = 0dB)
+	val = (0 << BIT_RF_DR_HIGH) | (3 << BIT_RF_PWR); //0x07;
+	WriteRegister(REG_RF_SETUP, &val, 1); //0b0000 0111 bit 3="0" 1Mbps=longer range, bit 2-1 power mode ("11" = 0dB)
 	
 	// RX RF_Adress width setup 5 byte - set receiver address (set RX_ADDR_P0 = TX_ADDR if EN_AA is enabled)
 	// Address = 0x01 00 01 00 01
-	/*
-	val[0] = '1';
-	val[1] = 'N';
-	val[2] = 'o';
-	val[3] = 'd';
-	val[4] = 'e';
-	*/
-	// TO-DO: Make it configurable
-	val[0] = 0x01;
-	val[1] = 0x00;
-	val[2] = 0x01;
-	val[3] = 0x00;
-	val[4] = 0x01;
-	
 	// Since we chose pipe 0 and 1 on EN_RXADDR, we give this address to those channels.
 	// P1 is the primary receiver address.
 	// It is possible to assign different addresses to different channels (if they are enabled in EN_RXADDR) in order
 	// to listen on several different transmitters
-	RF24ReadWrite(W, RX_ADDR_P1, val, 5);
+	WriteRegister(REG_RX_ADDR_P0, Node_Address, DEFAULT_ADDRESS_SIZE);
 	
 	// TX RF_Adress setup 5 byte - set TX address
 	// Not used in a receiver but can be set anyway. Equal to Rx address
-	RF24ReadWrite(W, TX_ADDR, val, 5);
+	WriteRegister(REG_TX_ADDR, Node_Address, DEFAULT_ADDRESS_SIZE);
 	
-	// Enable dynamic payload length and payload with ack packet
-	val[0] = 0x06;
-	RF24ReadWrite(W, FEATURE, val, 1); // 0b0000 0110: EN_DPL = 1, EN_ACK_PAY = 1
-	
-	// Choose the pipes with dynamic payload length. 0 and 1.
-	val[0] = 0x03;
-	RF24ReadWrite(W, DYNPD, val, 1);
+//	// Enable dynamic payload length and payload with ack packet
+//	val[0] = 0x06;
+//	WriteRegister(REG_FEATURE, val, 1); // 0b0000 0110: EN_DPL = 1, EN_ACK_PAY = 1
+//
+//	// Choose the pipes with dynamic payload length. 0 and 1.
+//	val[0] = 0x03;
+//	RF24ReadWrite(DYNPD, val, 1);
 	
 	/*
 	// Payload width setup - how many bytes to send per transmission (1-32)
@@ -90,13 +88,19 @@ void Radio__Initialize(void)
 	*/
 	
 	// CONFIG reg setup - boot up the nRF24L01 and choose if it is a transmitter or a receiver
-	// 0b0001 1111 - bit 0="0": transmitter bit 0="1":Receiver, bit 1="1":power up,  
-	// bit 4="1": mask_Max_RT i.e. IRQ-interrupt is not triggered if transmission failed
-	val[0] = 0x1F;  
-	RF24ReadWrite(W, CONFIG, val, 1);
+	val = (0 << BIT_PRIM_RX) | (0 << BIT_PWR_UP) | (1 << BIT_MASK_MAX_RT);
+	WriteRegister(REG_CONFIG, &val, 1);
 	
 	//device need 1.5ms to reach standby mode
 	_delay_ms(100);
+
+	// INT0 init
+	DDRD &= ~(1 << DDD2);
+    // INT0 falling edge PD2
+    EICRA |=  (1<<ISC01);
+    EICRA  &=  ~(1<<ISC00);
+    // Enable IRQ INT0
+    EIMSK |=  (1<<INT0);
 }
 
 /**
@@ -109,14 +113,11 @@ void Radio__Initialize(void)
 uint8_t RF24GetReg(uint8_t reg)
 {
 	_delay_us(10);
-	PORTB &= ~(1 << PORTB2);	// CSN low
-	_delay_us(10);
 	Spi__PutChar(R_REGISTER + reg);	// R_REGISTER = set the RF24 to reading mode, "reg" is the register that will be read back
 	_delay_us(10);
 	Spi__WriteThenRead(NOP); // Send a NOP (dummy byte) in order to read the first byte of the register "reg"
 	reg = Spi__GetChar();
 	_delay_us(10);
-	PORTB |= (1 << PORTB2); //CSN IR_High
 	return reg;	// Return the read register
 }
 
@@ -145,8 +146,6 @@ uint8_t *RF24ReadWrite(uint8_t ReadWrite, uint8_t reg, uint8_t *val, uint8_t nVa
 
 	//! Makes sure we wait a bit
 	_delay_us(10);
-	PORTB &= ~(1 << PORTB2);	// CSN low
-	_delay_us(10);
 	Spi__PutChar(reg);	// set to write or read mode the register "reg"
 	_delay_us(10);
 
@@ -167,8 +166,6 @@ uint8_t *RF24ReadWrite(uint8_t ReadWrite, uint8_t reg, uint8_t *val, uint8_t nVa
 			_delay_us(10);
 		}
 	}
-	PORTB |= (1 << PORTB2);	//CSN IR_High
-
     }
 
 	return ret;
@@ -225,16 +222,51 @@ void RF24ReceivePayload(void)
  */
 void RF24ResetIRQ(void)
 {
-	_delay_us(10);
-	// CSN low
-	PORTB &= ~(1 << PORTB2);
-	_delay_us(10);
 	// Write to STATUS register
 	Spi__PutChar(W_REGISTER + STATUS);
 	_delay_us(10);
 	// Reset all IRQ in STATUS register
 	Spi__PutChar(0x70);
 	_delay_us(10);
-	// CSN high
-	PORTB |= (1 << PORTB2);
+}
+
+static void WriteRegister(uint8_t reg, uint8_t* val, uint8_t n_val)
+{
+    uint8_t i;
+
+    reg = CMD_W_REGISTER + reg;
+    Spi__PutChar(reg);
+    for (i = 0; i < n_val; i++)
+    {
+        Spi__PutChar(val[i]);
+    }
+}
+
+/**
+ * @brief ISR on INT0
+ *
+ * This is called when successful data receive or transmission
+ * happened
+ *
+ * @todo Avoid 0.5 sec delay
+ *
+ * @return void
+ */
+ISR(INT0_vect)
+{
+    PORTB &= ~(1 << PORTB1);
+
+    // LED to show success
+    PORTB |= (1 << PORTB0);
+    _delay_ms(500);
+    PORTB &= ~(1 << PORTB0);
+    // Read data from RX FIFO
+    data = RF24ReadWrite(R, R_RX_PAYLOAD, data, 32);
+    // Print data to console
+    for (int i = 0; i < 32; i++)
+    {
+        Usart__PutChar(data[i]);
+    }
+    // Clear IRQ masks in STATUS register
+    RF24ResetIRQ();
 }
